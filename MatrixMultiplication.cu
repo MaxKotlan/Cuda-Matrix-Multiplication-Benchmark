fbcd2a410cb5225995bb995019e14697195bc752
max <max.maxkot@gmail.com>
2020-01-25 03:19:40 -0500

    Frees Memory. Tests range of matrices.

--- MatrixMultiplication.cu
+++ MatrixMultiplication.cu
@@ -4,21 +4,26 @@
 #include <curand_kernel.h>
 #include <time.h>
 
-__global__ void kernel(void){
-
-}
-
+/*Data structure, which holds a pointer to the elements 
+of the matrix, and the number of rows/columns*/
 struct squareMatrix{
     int* elements;
     int dimension;
+
+    int* dev_elements; //pointer to elements copied over to the gpu
+};
+
+__host__ void freeMemory(squareMatrix mat){
+    free(mat.elements);
+    cudaFree(mat.dev_elements);
 };
 
 __host__ squareMatrix createRandomSquareMatrix(int dimension){
     int  mat_elements = dimension * dimension;
     int* mat = (int*)malloc(sizeof(int)*mat_elements);
     for (int i = 0; i < mat_elements; i++)
-        mat[i] = rand()%10;
-    return {mat, dimension};
+        mat[i] = rand()%2;
+    return {mat, dimension, nullptr};
 }
 
 __host__ void printSquareMatrix(squareMatrix mat){
@@ -31,7 +36,7 @@ __host__ void printSquareMatrix(squareMatrix mat){
 
 __host__ squareMatrix multiplyMatrices(squareMatrix a, squareMatrix b){
     if (a.dimension != b.dimension) exit(1);
-    squareMatrix result = { (int*)malloc(sizeof(int)*a.dimension*a.dimension), a.dimension };
+    squareMatrix result = { (int*)malloc(sizeof(int)*a.dimension*a.dimension), a.dimension, nullptr };
     
     for (int i = 0; i < result.dimension; i++){
         for (int j = 0; j < result.dimension; j++){
@@ -43,23 +48,40 @@ __host__ squareMatrix multiplyMatrices(squareMatrix a, squareMatrix b){
     return result;
 }
 
-int main(void) {
+__global__ void multiplyMatricesParallel(void){
+    
+}
 
-    squareMatrix mat_a = createRandomSquareMatrix(1000);
-    squareMatrix mat_b = createRandomSquareMatrix(1000);
-
-    //printSquareMatrix(mat_a);
-    //printSquareMatrix(mat_b);
-
+__host__ void testMatrixMultiplicationPreformance(int dimension){
+    /*Create Two Random NxN Matrices*/
+    squareMatrix mat_a = createRandomSquareMatrix(dimension);
+    squareMatrix mat_b = createRandomSquareMatrix(dimension);
+    
     clock_t before = clock();
     squareMatrix mat_c = multiplyMatrices(mat_a, mat_b);
     clock_t totaltime = clock() - before;
     int msec = totaltime * 1000 / CLOCKS_PER_SEC;
-    printf("%d msec", msec);
+    printf("Multiplying %dx%d X %dx%d took %d msec using the CPU\n--------------------------------------------------------------------------------------------\n", 
+    dimension, dimension, dimension, dimension, msec);
 
-    //printSquareMatrix(mat_c);
+    cudaMalloc((void**)&mat_a.dev_elements, mat_a.dimension*mat_a.dimension*sizeof(int));
+    cudaMalloc((void**)&mat_b.dev_elements, mat_b.dimension*mat_b.dimension*sizeof(int));
 
-    kernel<<<1,1>>> ();
-    //printf("Hello World\n");
+    cudaMemcpy(mat_a.elements, mat_a.dev_elements, mat_a.dimension*mat_a.dimension*sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(mat_b.elements, mat_b.dev_elements, mat_b.dimension*mat_b.dimension*sizeof(int), cudaMemcpyHostToDevice);
+
+    multiplyMatricesParallel<<<1,1>>> ();
+
+    freeMemory(mat_a);
+    freeMemory(mat_b);
+    freeMemory(mat_c);
+}
+
+
+int main(void) {
+
+    for (int i = 16; i < 1025; i*=2 )
+        testMatrixMultiplicationPreformance(i);
+
 	return 0;
 }
