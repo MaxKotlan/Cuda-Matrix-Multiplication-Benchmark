2985e65cef80d198c3163d20c45d5b060d80f231
max <max.maxkot@gmail.com>
2020-01-25 04:08:32 -0500

    currently not working. Ruined cpu benchmark

--- MatrixMultiplication.cu
+++ MatrixMultiplication.cu
@@ -9,13 +9,6 @@ of the matrix, and the number of rows/columns*/
 struct squareMatrix{
     int* elements;
     int dimension;
-
-    int* dev_elements; //pointer to elements copied over to the gpu
-};
-
-__host__ void freeMemory(squareMatrix mat){
-    free(mat.elements);
-    cudaFree(mat.dev_elements);
 };
 
 __host__ squareMatrix createRandomSquareMatrix(int dimension){
@@ -23,7 +16,7 @@ __host__ squareMatrix createRandomSquareMatrix(int dimension){
     int* mat = (int*)malloc(sizeof(int)*mat_elements);
     for (int i = 0; i < mat_elements; i++)
         mat[i] = rand()%2;
-    return {mat, dimension, nullptr};
+    return {mat, dimension};
 }
 
 __host__ void printSquareMatrix(squareMatrix mat){
@@ -36,7 +29,7 @@ __host__ void printSquareMatrix(squareMatrix mat){
 
 __host__ squareMatrix multiplyMatrices(squareMatrix a, squareMatrix b){
     if (a.dimension != b.dimension) exit(1);
-    squareMatrix result = { (int*)malloc(sizeof(int)*a.dimension*a.dimension), a.dimension, nullptr };
+    squareMatrix result = { (int*)malloc(sizeof(int)*a.dimension*a.dimension), a.dimension };
     
     for (int i = 0; i < result.dimension; i++){
         for (int j = 0; j < result.dimension; j++){
@@ -48,33 +41,59 @@ __host__ squareMatrix multiplyMatrices(squareMatrix a, squareMatrix b){
     return result;
 }
 
-__global__ void multiplyMatricesParallel(void){
+__global__ void multiplyMatricesParallel(int* mat_a, int* mat_b, int* mat_results, int dimension){
     
 }
 
-__host__ void testMatrixMultiplicationPreformance(int dimension){
-    /*Create Two Random NxN Matrices*/
-    squareMatrix mat_a = createRandomSquareMatrix(dimension);
-    squareMatrix mat_b = createRandomSquareMatrix(dimension);
-    
-    clock_t before = clock();
-    squareMatrix mat_c = multiplyMatrices(mat_a, mat_b);
-    clock_t totaltime = clock() - before;
+__host__ void printTime(clock_t totaltime, int dimension){
     int msec = totaltime * 1000 / CLOCKS_PER_SEC;
     printf("Multiplying %dx%d X %dx%d took %d msec using the CPU\n--------------------------------------------------------------------------------------------\n", 
     dimension, dimension, dimension, dimension, msec);
+}
 
-    cudaMalloc((void**)&mat_a.dev_elements, mat_a.dimension*mat_a.dimension*sizeof(int));
-    cudaMalloc((void**)&mat_b.dev_elements, mat_b.dimension*mat_b.dimension*sizeof(int));
+/*
+ Found on the stack overflow:  https://stackoverflow.com/questions/14038589/what-is-the-canonical-way-to-check-for-errors-using-the-cuda-runtime-api
+ Throws errors if cuda command doesn't return Success
+*/
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess) 
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
+}
 
-    cudaMemcpy(mat_a.elements, mat_a.dev_elements, mat_a.dimension*mat_a.dimension*sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(mat_b.elements, mat_b.dev_elements, mat_b.dimension*mat_b.dimension*sizeof(int), cudaMemcpyHostToDevice);
+__host__ void testMatrixMultiplicationPreformance(int dimension){
 
-    multiplyMatricesParallel<<<1,1>>> ();
+    squareMatrix        mat_a,        mat_b,        mat_results;
+    squareMatrix device_mat_a, device_mat_b, device_mat_results;
+    
+    mat_a = createRandomSquareMatrix(dimension);
+    mat_b = createRandomSquareMatrix(dimension);
+    
+    clock_t before = clock();
+    mat_results = multiplyMatrices(mat_a, mat_b);
+    printTime(clock() - before, mat_results.dimension);
 
-    freeMemory(mat_a);
-    freeMemory(mat_b);
-    freeMemory(mat_c);
+
+    gpuErrchk(cudaMalloc(&device_mat_a.elements, dimension*dimension*sizeof(int)));
+    gpuErrchk(cudaMalloc(&device_mat_b.elements, dimension*dimension*sizeof(int)));
+    gpuErrchk(cudaMalloc(&device_mat_results.elements, dimension*dimension*sizeof(int)));
+    
+    gpuErrchk(cudaMemcpy(device_mat_a.elements, &mat_a.elements, dimension*dimension*sizeof(int), cudaMemcpyHostToDevice));
+    gpuErrchk(cudaMemcpy(device_mat_a.elements, &mat_b.elements, dimension*dimension*sizeof(int), cudaMemcpyHostToDevice));
+
+    multiplyMatricesParallel<<<1,1024>>> (device_mat_a.elements, device_mat_b.elements, device_mat_results.elements, dimension);
+
+    free(mat_results.elements);
+    gpuErrchk(cudaMemcpy(&device_mat_results.elements, mat_results.elements, dimension*dimension*sizeof(int), cudaMemcpyDeviceToHost));
+
+
+    free(mat_a.elements);
+    free(mat_b.elements);
+    free(mat_results.elements);
 }
 
 
